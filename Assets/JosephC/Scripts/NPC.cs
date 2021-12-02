4ab4b2e3452afa908622fa73d164a93ff682ce2c
MUKILTEO\1710347 <lolfunnymanhahahaha@gmail.com>
2021-12-02 12:51:07 -0800

    texbox

---
+++ Assets/JosephC/Scripts/NPC.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    public object[,] text = { { "hi", 0, true } };
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public object[,] InterAct()
+    {
+        return text;
+    }
+}
--- Assets/JosephC/Scripts/textbox.cs
+++ Assets/JosephC/Scripts/textbox.cs
@@ -6,17 +6,23 @@ public class textbox : MonoBehaviour
 {
     
     int curtext = 0;
-    public GameObject texbox;
+    GameObject texobj;
+    private void Start()
+    {
+        texobj = transform.GetChild(0).gameObject;
+    }
     public void DoText(object[,] text)
     {
         string line = text[curtext, 0] as string;
-        string icon = text[curtext, 1] as string;
+        int icon = (int) text[curtext, 1];
         bool CanEnd = (bool) text[curtext, 2];
 
         print((icon, line, CanEnd));
+
         if (CanEnd)
         {
             curtext = 0;
+            
         }
         else
         {
