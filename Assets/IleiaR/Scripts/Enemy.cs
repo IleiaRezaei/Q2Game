dff782eb31abac6b4c9e54fa531b83fa7bd5a561
MUKILTEO\1710197 <ileia.rezaei2005@gmail.com>
2021-12-07 13:25:43 -0800

    12/7 last push

---
+++ Assets/IleiaR/Scripts/Enemy.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public int health;
+    public int maxhealth = 100;
+
+    public int attackDamage;
+
+    private Rigidbody2D rb;
+   
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        //GameObject.Find("Player").GetComponent<CharacterControll>();
+        //GetComponent<CharacterControll>().attacking
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "PlayerAttack" && attack = true)
+        {
+            int dam = collision.gameObject.transform.parent.gameObject.GetComponent<CharacterControll>().damage;
+            health -= dam;
+        }
+    }
+}
