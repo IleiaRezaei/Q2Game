c8041f7387f6535ce1d45d1adbbacac2b0dad44e
MUKILTEO\1710197 <ileia.rezaei2005@gmail.com>
2021-12-13 13:26:36 -0800

    enemy boom

---
+++ Assets/IleiaR/Scripts/PotatoEnemy.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotatoEnemy : MonoBehaviour
+{
+
+    public Sprite sprite1;
+    public Sprite sprite2;
+
+    private SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = sprite1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ChangeColor()
+    {
+        if(spriteRenderer.sprite == sprite1)
+        {
+            spriteRenderer.sprite = sprite2;
+        }
+        else
+        {
+            spriteRenderer.sprite = sprite1;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player" || collision.gameObject.tag == "PlayerAttack")
+        {
+            ChangeColor();
+            Invoke("ChangeColor", 1);
+        }
+    }
+}
