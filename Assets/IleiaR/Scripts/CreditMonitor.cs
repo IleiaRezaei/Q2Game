065895f67b5a495925dc8b98851324d02e9f63cc
MUKILTEO\1710197 <ileia.rezaei2005@gmail.com>
2021-12-14 12:48:45 -0800

    Credit scene and a escape to go back scene

--- Assets/IleiaR/Scripts/CreditMonitor.cs
+++ Assets/IleiaR/Scripts/CreditMonitor.cs
@@ -5,23 +5,41 @@ using UnityEngine.SceneManagement;
 
 public class CreditMonitor : MonoBehaviour
 {
+    private static int nScreens = 7;
+    private GameObject[] creditScenes = new GameObject[nScreens];
+    private static int swapCount = 0;
 
     // Use this for initialization
     void Start()
     {
+        creditScenes[0] = GameObject.Find("Credits1");
+        creditScenes[1] = GameObject.Find("Credits2");
+        creditScenes[2] = GameObject.Find("Credits3");
+        creditScenes[3] = GameObject.Find("Credits4");
+        creditScenes[4] = GameObject.Find("Credits5");
+        creditScenes[5] = GameObject.Find("Credits6");
+        creditScenes[6] = GameObject.Find("Credits7");
 
+        //Turn all scenes off
+        for(int i = 0; i < nScreens; i++)
+        {
+            creditScenes[i].SetActive(false);
+        }
+
+        creditScenes[0].SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-
-
         if (Input.GetKeyDown(KeyCode.C))
         {
-
-            SceneManager.LoadScene("CreditScene"); //Requires "Using" (see above)
-
+            int CurrentScene = swapCount % nScreens;
+            creditScenes[CurrentScene].SetActive(false);
+            swapCount++;
+            CurrentScene = swapCount % nScreens;
+            creditScenes[CurrentScene].SetActive(true);
+            Debug.Log(CurrentScene);
         }
     }
 }
